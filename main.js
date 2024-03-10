1119f9303c414f10620c5bb58c83035719dd1ac1
agustinbelisle <151087066+agustinbelisle@users.noreply.github.com>
2024-03-10 11:38:45 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const pizzasIdImpar = pizzas.filter(pizza => pizza.id % 2 !== 0);
+console.log("Pizzas con ID impar:");
+console.log(pizzasIdImpar);
+
+
+
+const pizzaMenor600 = pizzas.some(pizza => pizza.precio < 600);
+console.log("¿Hay alguna pizza que valga menos de $600?");
+console.log(pizzaMenor600 ? "Sí, hay pizzas que valen menos de $600." : "No, todas las pizzas valen $600 o más.");
+
+console.log("Nombre de cada pizza con su respectivo precio:");
+pizzas.forEach(pizza => {
+  console.log(`${pizza.nombre} - Precio: $${pizza.precio}`);
+});
+
+console.log("Ingredientes de cada pizza:");
+pizzas.forEach(pizza => {
+  console.log(`Ingredientes de ${pizza.nombre}:`);
+  pizza.ingredientes.forEach(ingrediente => {
+    console.log(ingrediente);
+  });
+});
+
---
+++ main2.js
@@ -0,0 +1,4 @@
+const pizzasIdImpar = pizzas.filter(pizza => pizza.id % 2 !== 0);
+console.log("Pizzas con ID impar:");
+console.log(pizzasIdImpar);
+
